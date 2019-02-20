1b9ce237fe8daf31a70239f46b736c61279b64df
Augusto Batista <augusto.batista@ou.edu>
2019-02-19 18:28:24 -0600

    Completed last task

--- src/Todo.java
+++ src/Todo.java
@@ -1,18 +1,24 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 11
+Last Modified_______: 2019-02-19
+OU ID_______________: 113475773
+OU EMAIL____________: augusto.batista@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Complete parseAndExecute method 
+2. execute Method
+3. calculation of two tokens
+4. calculation of three tokens
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 45
+2. 20
+3. 10
+4. 8
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 139
+2. 19
+3. 4
+4. 6
 */}
